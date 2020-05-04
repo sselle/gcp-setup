e3c2ed1cc83277c965d0d177d1ccfa92ffa38fe0
Sven Selle <s.selle@web.de>
2020-05-04 18:15:59 +0200

    added feature to list all buckets in project defined by local key

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -1,4 +1,27 @@
+from google.cloud import storage
 import click
 
+@click.group()
+def cli():
+    global storage_client
+    storage_client = storage.Client()
+
+@cli.command('list-instances')
+def list_instances():
+    """List all instances in current project"""
+    print('look at all my instances!')
+
+@cli.command('create-instance')
+def create_instance():
+    """create a new instance in current project"""
+    print('here we will create a new instance later')
+
+@cli.command('list-buckets')
+def list_buckets():
+    """list all buckets in GCP project"""
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+
 if __name__ == '__main__':
-    print('hello world')
+    cli()
