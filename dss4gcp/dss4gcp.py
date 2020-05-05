084dd1316892665ded906b2197eee36e94d1010c
Sven Selle <s.selle@web.de>
2020-05-05 14:45:38 +0200

    change name to dss4gcp

---
+++ dss4gcp/dss4gcp.py
@@ -0,0 +1,34 @@
+import click
+
+from storage_manager import StorageManager
+from compute_manager import ComputeManager
+
+@click.group()
+def cli():
+    """dss4gcp supports the setup of gcp infrastrucutre"""
+    global storage, compute
+    storage = StorageManager()
+    compute = ComputeManager(project='dataiku-cluster', zone='us-central1-a')
+
+@cli.command('list-instances')
+def list_instances():
+    """List all instances in current project"""
+    for instance in compute.all_instances()['items']:
+        print(instance['name'])
+
+
+@cli.command('create-instance')
+def create_instance():
+    """create a new instance to run DSS"""
+    print('here we will create a new instance later')
+
+
+@cli.command('list-buckets')
+def list_buckets():
+    """list all buckets in GCP project"""
+    for bucket in storage.all_buckets():
+        print(bucket.name)
+
+
+if __name__ == '__main__':
+    cli()
